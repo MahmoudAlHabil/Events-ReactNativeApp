cff546b5dde05a55211c1039de6db61c9dbf3218
Mahmoud AlHabil <mahmoudalhabil2001@gmail.com>
2022-12-09 15:33:57 +0200

    Add colors and icons files

---
+++ src/utils/colors.js
@@ -0,0 +1,54 @@
+const colors = {
+    primary: {
+        dark: "#256D85",
+        main: "#FF6D28",
+        assist: "#FFC19E",
+        light: "#FFE9DF",
+        BG: "#FFE6D6",
+    },
+    secondary: {
+        dark: "#02323D",
+        main: "#17505C",
+        assist: "#A4C1C8",
+        light: "#D1E0E3",
+        BG: "#EDF3F4",
+    },
+    danger: {
+        dark: "#B91C1C",
+        main: "#EF4444",
+        BG: "#FEE2E2",
+    },
+    warning: {
+        dark: "#C2850A",
+        main: "#F2A60D",
+        BG: "#FDF7EB",
+    },
+    success: {
+        dark: "#005005",
+        main: "#087F23",
+        BG: "#E7F3E7",
+    },
+    info: {
+        dark: "#005CB2",
+        main: "#1E88E5",
+        BG: "#E9F4FF",
+    },
+    gray: {
+        50: "#F9FAFB",
+        100: "#F3F4F6",
+        200: "#E5E7EB",
+        300: "#D1D5DB",
+        400: "#9CA3AF",
+        500: "#6B7280",
+        600: "#4B5563",
+        700: "#374151",
+        800: "#1F2937",
+        900: "#111827",
+    },
+    common: {
+        black: "#000000",
+        white: "#FFFFFF",
+    },
+};
+
+export default colors;
